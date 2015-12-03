32dff55d606ea875d138e4a9a4cd27055624753c
Chris Knight <chrisknight@ucla.edu>
2015-12-03 15:57:33 -0800

    Only do default module install if no requirements files are present.

--- rpmvenv/extensions/python/venv.py
+++ rpmvenv/extensions/python/venv.py
@@ -108,18 +108,23 @@ class Extension(interface.Extension):
         spec.blocks.files.append('/%{venv_install_dir}')
 
         spec.blocks.install.append('%{venv_cmd} %{venv_dir}')
-        for requirement in config.python_venv.requirements:
-
-            spec.blocks.install.append(
-                '%{{venv_pip}} -r %{{SOURCE0}}/{0}'.format(
-                    requirement,
+        # If we have requirements files, install based on them.
+        if len(tuple(config.python_venv.requirements)) > 0:
+            for requirement in config.python_venv.requirements:
+                spec.blocks.install.append(
+                    '%{{venv_pip}} -r %{{SOURCE0}}/{0}'.format(
+                        requirement,
+                    )
                 )
-            )
+        # Otherwise, use standard Python install.
+        else:
+            spec.blocks.install.extend((
+                'cd %{SOURCE0}',
+                '%{venv_python} setup.py install',
+                'cd -',
+            ))
 
         spec.blocks.install.extend((
-            'cd %{SOURCE0}',
-            '%{venv_python} setup.py install',
-            'cd -',
             '# RECORD files are used by wheels for checksum. They contain path'
             ' names which',
             '# match the buildroot and must be removed or the package will '
