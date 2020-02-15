675fbbad8917e2ce2d9395c47a42650bd0e4f697
Kevin Conway <kevinjacobconway@gmail.com>
2020-02-14 18:13:11 -0600

    Add test to reproduce ordering issues
    This is an attempt to reproduce an issue reported related to the
    non-deterministic ordering of extensions.

---
+++ tests/test_extensions.py
@@ -0,0 +1,20 @@
+"""Test suites for the extension management features."""
+
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import itertools
+import pytest
+
+from rpmvenv.extensions import loader
+
+
+def test_loader_deterministic_order():
+    """Test that the extensions are always loaded in requested order."""
+    extensions = ("file_permissions", "file_extras", "python_venv", "blocks")
+    for selection in itertools.permutations(extensions):
+        results = loader.load_extensions(selection)
+        results = (result.name for result in results if result.name != "core")
+        assert tuple(results) == selection
