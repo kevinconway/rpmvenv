a84546897ff99346a32b22c6ef117578675b5c63
Kevin Conway <kevinjacobconway@gmail.com>
2021-12-29 22:15:22 -0600

    Bump PyPi version for nu shell updates

--- setup.py
+++ setup.py
@@ -4,39 +4,39 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-with open('README.rst', 'r') as readmefile:
+with open("README.rst", "r") as readmefile:
 
     README = readmefile.read()
 
 setup(
-    name='rpmvenv',
-    version='0.25.0',
-    url='https://github.com/kevinconway/rpmvenv',
-    description='RPM packager for Python virtualenv.',
+    name="rpmvenv",
+    version="0.26.0",
+    url="https://github.com/kevinconway/rpmvenv",
+    description="RPM packager for Python virtualenv.",
     author="Kevin Conway",
     author_email="kevinjacobconway@gmail.com",
     long_description=README,
-    license='MIT',
-    packages=find_packages(exclude=['tests', 'build', 'dist', 'docs']),
+    license="MIT",
+    packages=find_packages(exclude=["tests", "build", "dist", "docs"]),
     install_requires=[
-        'jinja2',
-        'venvctrl',
-        'argparse',
-        'confpy',
-        'ordereddict',
-        'semver<2.10',  # semver dropped support for legacy Python at 2.10.
+        "jinja2",
+        "venvctrl",
+        "argparse",
+        "confpy",
+        "ordereddict",
+        "semver<2.10",  # semver dropped support for legacy Python at 2.10.
     ],
     entry_points={
-        'console_scripts': [
-            'rpmvenv = rpmvenv.cli:main',
+        "console_scripts": [
+            "rpmvenv = rpmvenv.cli:main",
         ],
-        'rpmvenv.extensions': [
-            'core = rpmvenv.extensions.core:Extension',
-            'file_permissions = rpmvenv.extensions.files.permissions:Extension',
-            'file_extras = rpmvenv.extensions.files.extras:Extension',
-            'python_venv = rpmvenv.extensions.python.venv:Extension',
-            'blocks = rpmvenv.extensions.blocks.generic:Extension',
-        ]
+        "rpmvenv.extensions": [
+            "core = rpmvenv.extensions.core:Extension",
+            "file_permissions = rpmvenv.extensions.files.permissions:Extension",
+            "file_extras = rpmvenv.extensions.files.extras:Extension",
+            "python_venv = rpmvenv.extensions.python.venv:Extension",
+            "blocks = rpmvenv.extensions.blocks.generic:Extension",
+        ],
     },
     package_data={
         "rpmvenv": ["templates/*"],
